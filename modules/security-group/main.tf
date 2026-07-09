e1d1161138d6d22be84ed4cb64464d5939be3d16
Sohan Pinnarwar <mr.sohanpinnarwar@gmail.com>
2026-07-09 09:59:29 +0530

    fix: allow EC2 Instance Connect SSH

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -3,12 +3,22 @@ resource "aws_security_group" "bastion_sg" {
   description = "Allow SSH access to Bastion Host"
   vpc_id      = var.vpc_id
 
+  # SSH from your own public IP
   ingress {
-    description = "Allow SSH"
+    description = "Allow SSH from my IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  # SSH from AWS EC2 Instance Connect
+  ingress {
+    description     = "Allow EC2 Instance Connect"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    prefix_list_ids = ["pl-0e4bcff02b13bef1e"]
   }
 
   egress {
